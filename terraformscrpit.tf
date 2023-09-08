0b350e2301887d8d4a8bf01761164c63de1b00ac
AkashSharma-2023 <sharmaakashrock2023@gmail.com>
2023-09-08 11:24:58 +0530

    added a new scrpit file

---
+++ terraformscrpit.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
